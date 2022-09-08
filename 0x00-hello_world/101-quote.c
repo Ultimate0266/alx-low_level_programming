2d40724ae1da4ee552c61cfb6ab4435900ec9eca
Saefullah Sadiq <sadiqsaefullah@gmail.com>
2022-09-08 14:54:26 -0700

    my first C programming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the phrase "and that piece of art is useful"
+ * - Dora Korpar,
+ *   2015-10-19
+ *   Return: 1 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("and that piece of art is useful" - Dora Korpar, 2015-10-19,\n");
+	return (1);
+}
