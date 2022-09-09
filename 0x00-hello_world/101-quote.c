6777dd327edc4204c7cb29c40f1293f2e84c4e5b
Saefullah Sadiq <sadiqsaefullah@gmail.com>
2022-09-08 22:10:46 -0700

    my first C programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <unistd.h>
+
 /**
- * main - prints the phrase "and that piece of art is useful"  - Dora Korpar, 2015-10-19",
- * followed by a new line, to standard error.
- * Return: Always 1.
+ * main - prints a standard error
+ *
+ * Return: Always 1 (success)
  */
+
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
