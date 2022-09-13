1e7ba77ae3254b2c6149347c9ddb7b315857459d
Saefullah Sadiq <sadiqsaefullah@gmail.com>
2022-09-13 06:33:43 -0700

    nested loop functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char r;
+
+	for (r = 'a'; r <= 'z'; r++)
+	{
+		_putchar(r);
+	}
+	_putchar('\n');
+}
