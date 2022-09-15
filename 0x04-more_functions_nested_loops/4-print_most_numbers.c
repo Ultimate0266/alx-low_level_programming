107c78c44199b65a67a6b29b932133616f8a2b73
Saefullah Sadiq <sadiqsaefullah@gmail.com>
2022-09-15 09:37:16 -0700

    more loop functions

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_most_numbers - prints the numbers, from 0 to 9
  * from 2 and 4 and you can
- * only use _putchar twice 
+ * only use _putchar twice
  * Return: Always 0 (success)
  */
 
