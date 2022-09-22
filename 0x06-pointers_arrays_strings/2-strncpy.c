c17c6aa4f31de52beb7a4fcfc6b6914081b5035a
Saefullah Sadiq <sadiqsaefullah@gmail.com>
2022-09-22 04:14:07 -0700

    more on pointers arrays strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * *_strncat - function that concatenates two strings
- * Copies at most an inputted number
+ * *_strncat - Copies at most an inputted number
  * of bytes from string src into dest.
  * @dest: The buffer storing the string copy.
  * @src: The source string.
