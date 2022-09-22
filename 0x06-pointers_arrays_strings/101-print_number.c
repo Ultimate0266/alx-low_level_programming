5d9b54eb9d2c0a2f46674abacd5a850527856185
Saefullah Sadiq <sadiqsaefullah@gmail.com>
2022-09-22 15:21:16 -0700

    more on pointers arrays strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,7 +4,6 @@
  * print_number - function that prints an integer
  * @n: integer to be printed;
  */
-
 void print_number(int n)
 {
 	unsigned int n1;
@@ -17,9 +16,7 @@ void print_number(int n)
 	{
 		n1 = n;
 	}
-
 	if (n1 / 10)
 		print_number (n1 / 10);
-
 	_putchar((n1 % 10) + '0');
 }
