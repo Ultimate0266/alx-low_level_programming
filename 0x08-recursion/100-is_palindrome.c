dfb5d1fd547f51d97ab0ac0ad4b7d87619f81c6d
Saefullah Sadiq <sadiqsaefullah@gmail.com>
2022-09-28 06:31:12 -0700

    C - recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -44,7 +44,7 @@ int check_palindrome(char *s, int len, int index)
 /**
  * is_palindrome - Checks if a string is a palindrome.
  * @s: The string to be checked.
- * 
+ *
  * Return: If the string is a palindrome - 1
  * If the string is not a palindrome - 0.
  */
@@ -55,6 +55,6 @@ int is_palindrome(char *s)
 
 	if (!(*s))
 		return (1);
-	
+
 	return (check_palindrome(s, len, index));
 }
