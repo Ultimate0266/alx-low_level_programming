8fcd826120066a8875461763423a19b24b122bae
Saefullah Sadiq <sadiqsaefullah@gmail.com>
2022-09-30 07:21:22 -0700

    argc and argv in C

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,32 +14,32 @@ int main(int argc, char *argv[])
 {
 	int num, j, result;
 	int coins[] = {25, 10, 5, 2, 1};
-e
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-e
+
 	num = atoi(argv[1]);
 	result = 0;
-e
+
 	if (num < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-e
+
 	for (j = 0; j < 5 && num >= 0; j++)
 	{
 		while (num >= coins[j])
 		{
 			result++;
-e
+
 			num -= coins[j];
 		}
 	}
-e
+
 	printf("%d\n", result);
 	return (0);
 }
