66e2f9f2ce929dcec03620cde0ff54e37147d0f6
Ultimate0266 <111071951+Ultimate0266@users.noreply.github.com>
2022-10-25 00:07:18 +0200

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,11 +2,11 @@
 #include "lists.h"
 
 /**
- * print_listint - Prints all the elements of a linked list
- * @h: The head of the linked list
- *
- * Return: The number of nodes of the linked list
- */
+  * print_listint - Prints all the elements of a linked list
+  * @h: The head of the linked list
+  *
+  * Return: The number of nodes of the linked list
+  */
 size_t print_listint(const listint_t *h)
 {
 	int count = 0;
