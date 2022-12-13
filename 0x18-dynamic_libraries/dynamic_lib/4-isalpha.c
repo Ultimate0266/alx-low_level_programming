e738aec260970e641f444d3f6ec702dd4c8cb936
Ultimate0266 <111071951+Ultimate0266@users.noreply.github.com>
2022-12-13 12:01:28 +0100

    dynamic lib

--- 0x18-dynamic_libraries/dynamic_lib/4-isalpha.c
+++ 0x18-dynamic_libraries/dynamic_lib/4-isalpha.c
@@ -1,1 +1,16 @@
-sdkncsbdb
+#include "main.h"
+
+/**
+ * _isalpha - checks if a character is an alphabet
+ * @c: the character to check
+ * Return: 1 if its an alphabet 0 otherwise;
+ */
+
+int _isalpha(int c)
+{
+	if ((c > 96 && c < 122) ||
+		(c > 64 && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
