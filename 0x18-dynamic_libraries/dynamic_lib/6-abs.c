c8adec1214dbda740d17baf1ed4744994236e4d8
Ultimate0266 <111071951+Ultimate0266@users.noreply.github.com>
2022-12-13 12:04:52 +0100

    dynamic lib

--- 0x18-dynamic_libraries/dynamic_lib/6-abs.c
+++ 0x18-dynamic_libraries/dynamic_lib/6-abs.c
@@ -1,1 +1,14 @@
-sbdhavs
+#include "main.h"
+
+/**
+ * _abs - get the absolute value of an int
+ * @n: The integer to check
+ * Return: The absolute value of n
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
